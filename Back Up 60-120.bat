d9c691c1e0397857d579e31721334cb1b98e4f9f
Franck S. <84309973+TheFrostSeeker@users.noreply.github.com>
2024-12-13 13:29:26 +0100

    Add files via upload

---
+++ Back Up 60-120.bat
@@ -0,0 +1,18 @@
+@echo off
+
+CD C:\ProgramTemp\
+
+mkdir "%date:~6,4%"
+CD "%date:~6,4%"
+
+mkdir "%date:~3,2%"
+CD "%date:~3,2%"
+
+mkdir "%date:~0,2%"
+CD "%date:~0,2%"
+
+(
+  echo CONNECT I 10.32.40.198
+  echo REMOTE
+  echo BACKUP 60-120 T TNC:\
+) | tnccmd
---
+++ Back Up Leader 7.bat
@@ -0,0 +1,18 @@
+@echo off
+
+CD C:\ProgramTemp\
+
+mkdir "%date:~6,4%"
+CD "%date:~6,4%"
+
+mkdir "%date:~3,2%"
+CD "%date:~3,2%"
+
+mkdir "%date:~0,2%"
+CD "%date:~0,2%"
+
+(
+  echo CONNECT I 192.168.32.183
+  echo REMOTE
+  echo BACKUP Leader_7 T TNC:\
+) | tnccmd
